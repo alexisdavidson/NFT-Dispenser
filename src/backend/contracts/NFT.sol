8549872d2c4229c54a3e9930850786c1c87e108b
Alexis Davidson <alexisdavidson00@gmail.com>
2022-10-18 19:29:35 +0800

    Implement random token minting

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -4,55 +4,49 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract NFT is ERC721A, Ownable {
+contract NFT is ERC721URIStorage, Ownable, VRFConsumerBaseV2 {
+    string public baseUri = "ipfs://QmNPi93oynwoi7NmHmW7ABTgpMfaQtmxKJoxmpXsJHNgGK/";
     string public constant uriSuffix = '.json';
-
-    uint256 public immutable max_supply = 9941;
-
-    uint public amountMintPerAccount = 1;
-
-    bool public whitelistEnabled = true;
-    address[] private whitelistedAddresses;
-
-    bool public publicSaleEnabled;
-
-    string private constant unkownNotRevealedUri = "Not revealed yet";
-    string[20] private unknownUris; // 20 unkown to be revealed one by one as the story progresses.
-
+    uint256 public immutable max_supply = 5000;
+    uint256 public amountMintPerAccount = 0; // 0 for unlimited
     uint256 public price;
+    uint256[] private availableTokens;
+    address[] private redeemedTokens;
     
-    event MintSuccessful(
-        address user
-    );
+    //VRF Chainlink **************************************************************************************
+    uint64 s_subscriptionId;
+    VRFCoordinatorV2Interface COORDINATOR;
+    address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D; // goerli - Change this depending current blockchain!
+    bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15; // goerli - Change this depending current blockchain!
+    uint32 callbackGasLimit = 200000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords =  1;
+    uint256[] public s_randomWords;
+    uint256 public s_requestId;
 
-    constructor(address teamAddress, address[] memory _usersToWhitelist) ERC721A("PORK NFT", "PN")
-    {
-        // Set whitelist
-        delete whitelistedAddresses;
-        whitelistedAddresses = _usersToWhitelist;
+    address[] private mintersQueue;
+    
+    event MintSuccessful(address user, uint256 tokenId);
+    event Redeem(address user, uint256 tokenId);
 
-        // Mint 333 NFTs for the team
-        _mint(teamAddress, 333);
+    constructor(address ownerAddress, uint64 subscriptionId) ERC721("PORK NFT", "PN") VRFConsumerBaseV2(vrfCoordinator) {
+        // Initialize Chainlink Coordinator
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_subscriptionId = subscriptionId;
 
-        // Set unkownUris
-        uint256 unknownUrisLength = unknownUris.length;
-        for (uint256 i = 0; i < unknownUrisLength;) {
-            unknownUris[i] = unkownNotRevealedUri;
-            unchecked { ++i; }
+        _transferOwnership(ownerAddress);
+        for(uint i = 1; i <= max_supply; i++) {
+            availableTokens.push(i);
+            redeemedTokens.push(address(0));
         }
-
-        // Transfer ownership
-        _transferOwnership(teamAddress);
     }
 
     function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
         require(_exists(_tokenId), 'ERC721Metadata: URI query for nonexistent token ');
-
-        if (_tokenId < 20 && isUnkownRevealed(_tokenId)) { // 20 first tokens are Unkowns
-            return unknownUris[_tokenId];
-        }
 
         string memory currentBaseURI = _baseURI();
         return bytes(currentBaseURI).length > 0
@@ -60,64 +54,35 @@ contract NFT is ERC721A, Ownable {
             : '';
     }
 
-    function mintExternal(address minter, uint256 quantity) public payable {
-        require(totalSupply() + quantity < max_supply, 'Cannot mint more than max supply');
-        require(publicSaleEnabled || isWhitelisted(address(msg.sender)), 'You are not whitelisted');
-        require(balanceOf(msg.sender) < amountMintPerAccount, 'Each address may only mint x NFTs!');
+    function mint() external payable {
+        require(availableTokens.length <= 0, 'No more tokens available!');
+        require(amountMintPerAccount == 0 || balanceOf(msg.sender) < amountMintPerAccount, 'Each address may only mint x NFTs!');
         require(msg.value >= getPrice(), "Not enough ETH sent; check price!");
-        _mint(minter, quantity);
-        emit MintSuccessful(msg.sender);
+        
+        requestRandomNumberForTokenId(msg.sender);
     }
 
-    function mint(uint256 quantity) external payable {
-        mintExternal(msg.sender, quantity);
+    function mintRandom(address minter, uint256 _random) private {
+        uint256 randomIndex = _random % availableTokens.length;
+        uint256 resultRandomTokenId = availableTokens[randomIndex];
+        availableTokens[randomIndex] = availableTokens[availableTokens.length - 1];
+        availableTokens.pop();
+        
+        _safeMint(minter, resultRandomTokenId);
+        emit MintSuccessful(minter, resultRandomTokenId);
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://Qmbx9io6LppmpvavX3EqZY8igQxPZh7koUzW3mPRLkLQir/";
-    }
-    
-    function baseTokenURI() public pure returns (string memory) {
-        return _baseURI();
-    }
+    function redeemAndBurn(uint256 _tokenId) external {
+        require(msg.sender == ownerOf(_tokenId), "You are not the owner of this token");
 
-    function contractURI() public pure returns (string memory) {
-        return "ipfs://QmWBjrx4QnwwLWzu1GosaLw1wv3ikvC5Tq7sJUcqEzr3So/";
+        redeemedTokens[_tokenId] = msg.sender;
+        _burn(_tokenId);
+        
+        emit Redeem(msg.sender, _tokenId);
     }
 
-    function setPublicSaleEnabled(bool _state) public onlyOwner {
-        publicSaleEnabled = _state;
-    }
-
-    function setWhitelistEnabled(bool _state) public onlyOwner {
-        whitelistEnabled = _state;
-    }
-
-    function whitelistUsers(address[] calldata _users) public onlyOwner {
-        delete whitelistedAddresses;
-        whitelistedAddresses = _users;
-    }
-
-    function isWhitelisted(address _user) public view returns (bool) {
-        uint256 whitelistedAddressesLength = whitelistedAddresses.length;
-        for (uint256 i = 0; i < whitelistedAddressesLength;) {
-            if (whitelistedAddresses[i] == _user) {
-                return true;
-            }
-            unchecked { ++i; }
-        }
-        return false;
-    }
-
-    function revealUnkown(uint256 _tokenId, string calldata tokenUri) public onlyOwner {
-        require(_tokenId < 20, "tokenId must be between 0 and 20");
-        require(!isUnkownRevealed(_tokenId), "unkown has already been revealed");
-
-        unknownUris[_tokenId] = tokenUri;
-    }
-
-    function isUnkownRevealed(uint256 _tokenId) public view returns(bool) {
-        return keccak256(abi.encodePacked((unknownUris[_tokenId]))) != keccak256(abi.encodePacked((unkownNotRevealedUri)));
+    function _baseURI() internal view override returns (string memory) {
+        return baseUri;
     }
 
     function getPrice() view public returns(uint) {
@@ -128,11 +93,42 @@ contract NFT is ERC721A, Ownable {
         price = _price;
     }
 
+    function setBaseUri(string calldata _baseUri) public onlyOwner {
+        baseUri = _baseUri;
+    }
+
     function setAmountMintPerAccount(uint _amountMintPerAccount) public onlyOwner {
         amountMintPerAccount = _amountMintPerAccount;
     }
 
     function withdraw() public onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
+    }
+    
+    function requestRandomNumberForTokenId(address _minter) private {
+        s_requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+
+        mintersQueue.push(_minter);
+    }
+
+    // Callback function when random number from Chainlink is generated
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        require(mintersQueue.length > 0, "No minter in queue");
+        s_randomWords = randomWords;
+        mintRandom(mintersQueue[mintersQueue.length - 1], s_randomWords[0]);
+        mintersQueue.pop();
+    }
+    
+    function getRedeemedTokens() public view returns (address[] memory) {
+        return redeemedTokens;
     }
 }
